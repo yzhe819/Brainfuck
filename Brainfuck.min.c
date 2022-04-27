686f178363cefe6697a2372a5067b89426e685bf
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-04-27 15:55:27 +1200

    fix: stack issue

--- Brainfuck.min.c
+++ Brainfuck.min.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-char s[30000] = {0}, c[10000]; int l = 0, s[100], sl = 0;
+char s[30000]={0}, c[10000]; char* p=s+10000; int l=0, stl, st[100];
 int main(int argc, char** argv) {
-  char* p = s + 10000; int i = 0, j, k; FILE* f = fopen(argv[1], "r");
-  while (fread(&c[l++], 1, 1, f) == 1){};
+  FILE* f=fopen(argv[1], "r"); while (fread(&c[l], 1, 1, f)==1) {l++;}
   fclose(f); setbuf(stdout, NULL);
-  while (i++ < l){
+  for (int i=stl=0, j, k; i<l; i++) {
     switch (c[i]) {
       case '+': (*p)++; break; case '>': p++; break;
       case '-': (*p)--; break; case '<': p--; break;
       case '.': putchar((int)(*p)); break;
-      case ',': (*p) = getchar(); while ((getchar()) != '\n'); break;
-      case ']': if (*p) i = s[--sl]-1; break;
-      case '[':
-        if (*p) {s[sl++] = i; continue; break;} 
-        k = i-1, j = 0; do {c[k] == '[' && j++; c[k] == ']' && j--;}
-        while (++k < l && j != 0); if (j == 0) {i = k; continue; break;}
+      case ',': (*p)=getchar(); while ((getchar())!='\n'); break;
+      case ']': if (*p) {i=st[--stl]-1;} break;
+      case '[': 
+        if (*p) {st[stl++]=i; break;} k=i, j=0; 
+        do {c[k]=='['&&j++; c[k]==']'&&j--;} while (++k<l&&j!=0);
+        if (j==0) {i=k; break;}
         fprintf(stderr, "%s:%dn", __FILE__, __LINE__); return 3; break;
-      default: break;
     }
-  };
+  }
   printf("\n"); system("pause"); return 0;
 }
