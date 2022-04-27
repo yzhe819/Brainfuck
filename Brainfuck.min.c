1e6b76d0f1215eb6f30fd1792500404534d67d91
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-04-27 15:58:08 +1200

    add: goto

--- Brainfuck.min.c
+++ Brainfuck.min.c
@@ -3,7 +3,7 @@
 char s[30000]={0}, c[10000]; char* p=s+10000; int l=0, stl, st[100];
 int main(int argc, char** argv) {
   FILE* f=fopen(argv[1], "r"); while (fread(&c[l], 1, 1, f)==1) {l++;}
-  fclose(f); setbuf(stdout, NULL);
+  setbuf(stdout, NULL);
   for (int i=stl=0, j, k; i<l; i++) {
     switch (c[i]) {
       case '+': (*p)++; break; case '>': p++; break;
@@ -12,11 +12,11 @@ int main(int argc, char** argv) {
       case ',': (*p)=getchar(); while ((getchar())!='\n'); break;
       case ']': if (*p) {i=st[--stl]-1;} break;
       case '[': 
-        if (*p) {st[stl++]=i; break;} k=i, j=0; 
-        do {c[k]=='['&&j++; c[k]==']'&&j--;} while (++k<l&&j!=0);
-        if (j==0) {i=k; break;}
-        fprintf(stderr, "%s:%dn", __FILE__, __LINE__); return 3; break;
+        if (*p) {st[stl++]=i; break;}  
+        k=i, j=0; do {c[k]=='['&&j++; c[k]==']'&&j--;} while (++k<l&&j!=0);
+        if (j==0) {i=k; break;} goto ERROR;
     }
   }
-  printf("\n"); system("pause"); return 0;
+  fclose(f); printf("\n"); system("pause"); return 0;
+  ERROR: fprintf(stderr, "%s:%dn", __FILE__, __LINE__); return 3;
 }
